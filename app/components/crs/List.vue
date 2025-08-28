4e51371c7342527a1a185a65943cb177100b9287
almaanvi <almaanvi@gmail.com>
2025-08-27 20:29:29 -0500

    Crs add Form, List and Butons

---
+++ app/components/crs/List.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n()
+
+const router = useRouter()
+const itemId = ref('')
+
+// const pageName = 'clients'
+// const pageTitle = t(`${ pageName }`)
+
+const title = ref('crList')
+const titleI18n = computed(() => t(title.value))
+const isLoading = ref(true)
+
+const { getCrs } = useCR()
+const { crs, pending } = await getCrs()
+
+const headers = ref([
+  { title: 'crId', key: 'crId' },
+  { title: 'WR Date', key: 'createdAt' },
+  { title: 'Client', key: 'client.name' },
+  // { title: 'Location', key: 'location' },
+  { title: 'Address', key: 'client.address' },
+  { title: 'By', key: 'createdBy' },
+  { title: 'Actions', key: 'actions', sortable: false, width: '90', align: 'center' }
+])
+
+const viewItem = async (item:any) => {
+  router.push({ path: `/crs/${ item._id }` })
+}
+
+onMounted(() => {
+  // Simula la carga de datos
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
+
+</script>
+
+<template>
+  <v-card>
+    <v-progress-linear absolute bottom
+      height="2"
+      :active="isLoading || pending"
+      :indeterminate="isLoading || pending"
+      color="primary"
+    ></v-progress-linear>
+    <v-toolbar density="compact">
+      <v-toolbar-title>{{ titleI18n }}</v-toolbar-title>
+      <CrsBtnSubmit />
+    </v-toolbar>
+    
+    <!-- @vue-expect-error -->
+    <v-data-table density="compact" :headers="headers" :items="crs">
+
+      <template v-slot:[`item.createdAt`]="{ item }">
+        <a-data-table-item-created-at :item="item" />
+      </template>
+
+      <!-- <template v-slot:[`item.name`]="{ item, value }">
+        <v-chip :color="item.color">
+          <span>{{ value }}</span>
+        </v-chip>
+      </template> -->
+
+      <template v-slot:[`item.createdBy`]="{ item, value }">
+        <a-data-table-item-created-by :item="item" :value="value" />
+      </template>
+
+      <template v-slot:[`item.actions`]="{ item }">
+
+        <a-data-table-item-action-btn :item="item" :itemId="itemId"
+          icon="mdi-circle-outline"
+          @on-action="viewItem(item)"
+        />
+
+        <CrsBtnSubmit action="edit" isIconBtn :textOnBtn="false" :itemData="item" />
+
+      </template>
+    </v-data-table>
+  </v-card>
+
+</template>
