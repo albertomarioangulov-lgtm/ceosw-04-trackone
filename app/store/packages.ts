278f499efff7c0feb065e67ba19717b800fb2a39
almaanvi <almaanvi@gmail.com>
2025-09-03 16:34:32 -0500

    app/store/packages. add packagesByCR functions

--- app/store/packages.ts
+++ app/store/packages.ts
@@ -3,11 +3,13 @@ import { defineStore } from "pinia";
 export const usePackagesStore = defineStore('packagesStore', () => {
   const packages = ref([])
   const packagesByWR = ref([])
+  const packagesByCR = ref([])
 
   return {
     // State
     packages,
     packagesByWR,
+    packagesByCR,
 
     // Actions
     setPackages(payload: any) {
@@ -16,6 +18,10 @@ export const usePackagesStore = defineStore('packagesStore', () => {
 
     setPackagesByWR(payload: any) {
       packagesByWR.value = payload
+    },
+
+    setPackagesByCR(payload: any) {
+      packagesByCR.value = payload
     }
   }
 })
