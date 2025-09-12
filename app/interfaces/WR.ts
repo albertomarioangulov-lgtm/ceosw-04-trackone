f55c1289965e01d7cb674ca129648e2d49276ccb
almaanvi <almaanvi@gmail.com>
2025-09-11 22:28:44 -0500

    WR interface, code improvements

--- app/interfaces/WR.ts
+++ app/interfaces/WR.ts
@@ -1,6 +1,18 @@
+
+interface Package {
+  trkgNum: string | null;
+  measures: {
+    l: number | null;
+    w: number | null;
+    h: number | null;
+  },
+  weight?: number | null;
+  notes?: string | null;
+}
+
 export interface WR {
   _id?: number;
   wrId?: number;
   client?: string;
-  packages?: [string]
+  packages?: Package[]
 }
