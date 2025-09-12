6cca9de883f22e04f3111b535cae2285b956ddc6
almaanvi <almaanvi@gmail.com>
2025-09-12 02:46:38 -0500

    server/api/packages/[id]/get code improvements

--- server/api/packages/[...id].get.ts
+++ server/api/packages/[...id].get.ts
@@ -9,6 +9,10 @@ export default defineEventHandler(async (event) => {
   const id = event.context.params!.id
   
   const data = await Package.findById( id )
+    .populate({ path: 'client', select: 'name address poboxid country state city seller',
+        populate: { path: 'seller', select: 'code seller_code' }
+    })
+    .populate({ path: 'wr', select: 'wrId' })
     .populate({ path: 'createdBy', select: 'name initials color' })
     .exec()
 
