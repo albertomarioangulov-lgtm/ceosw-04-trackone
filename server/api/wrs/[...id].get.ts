299c40cb1404e2264b0c1c16823666508b6fbedd
almaanvi <almaanvi@gmail.com>
2025-09-11 22:36:31 -0500

    server/api/wrs[id]/get code improvements

--- server/api/wrs/[...id].get.ts
+++ server/api/wrs/[...id].get.ts
@@ -1,4 +1,5 @@
 // import { getServerSession, getToken } from '#auth'
+import mongoose from 'mongoose'
 import WR from "~~/server/models/WR"
 
 export default defineEventHandler(async (event) => {
@@ -9,9 +10,50 @@ export default defineEventHandler(async (event) => {
 
   const id = event.context.params!.id
   
-  const data = await WR.findById( id )
-    .populate({ path: 'createdBy', select: 'name initials color' })
-    .exec()
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw createError({ statusCode: 400, statusMessage: 'ID de WR inválido' })
+  }
 
-    return data
+  const aggregationPipeline: any = [
+    { $match: { _id: new mongoose.Types.ObjectId(id) } },
+    // Populate client
+    {
+      $lookup: {
+        from: 'clients',
+        localField: 'client',
+        foreignField: '_id',
+        as: 'client'
+      }
+    },
+    { $unwind: { path: '$client', preserveNullAndEmptyArrays: true } },
+    // Populate createdBy
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'createdBy',
+        foreignField: '_id',
+        as: 'createdBy'
+      }
+    },
+    { $unwind: { path: '$createdBy', preserveNullAndEmptyArrays: true } },
+    // Populate packages (virtual)
+    {
+      $lookup: {
+        from: 'packages',
+        localField: '_id',
+        foreignField: 'wr',
+        as: 'packages'
+      }
+    },
+    // Shape createdBy to select specific fields, mimicking .populate({ select: ... })
+    {
+      $addFields: {
+        'createdBy': { _id: '$createdBy._id', name: '$createdBy.name', initials: '$createdBy.initials', color: '$createdBy.color' }
+      }
+    }
+  ]
+
+  const [data] = await WR.aggregate(aggregationPipeline).exec()
+
+  return data
 })
