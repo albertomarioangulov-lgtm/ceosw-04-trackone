7827f9f7a57ae656798a3bbe304e69e61ff1ee0c
almaanvi <almaanvi@gmail.com>
2025-10-02 17:57:35 -0500

    name correction

---
+++ server/api/debug/check-secret.get.ts
@@ -0,0 +1,19 @@
+import getUserId from "~~/server/libs/userData"
+
+export default defineEventHandler(async (event) => {
+  // Aseguramos que solo un usuario autenticado pueda acceder
+
+  getUserId(event)
+
+  const config = useRuntimeConfig()
+
+  // const secret = process.env.NUXT_AUTH_SECRET;
+  const secret = config.authSecret
+
+  return {
+    secretExists: !!secret,
+    secretType: typeof secret,
+    secretLength: secret?.length || 0,
+    firstChars: secret?.substring(0, 4) || 'N/A'
+  }
+})
