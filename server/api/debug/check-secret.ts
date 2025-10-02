1d779dbb87be3f399be9ada4c996e25a5efb92b9
almaanvi <almaanvi@gmail.com>
2025-10-02 17:42:18 -0500

    test check secret

---
+++ server/api/debug/check-secret.ts
@@ -0,0 +1,15 @@
+import getUserId from "~~/server/libs/userData"
+
+export default defineEventHandler(async (event) => {
+  // Aseguramos que solo un usuario autenticado pueda acceder
+  getUserId(event)
+
+  const secret = process.env.NUXT_AUTH_SECRET;
+
+  return {
+    secretExists: !!secret,
+    secretType: typeof secret,
+    secretLength: secret?.length || 0,
+    firstChars: secret?.substring(0, 4) || 'N/A'
+  }
+})
